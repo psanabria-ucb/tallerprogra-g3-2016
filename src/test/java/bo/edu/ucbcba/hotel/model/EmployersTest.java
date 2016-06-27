6c3240c393317f39d67fc4d06a283d21d827b026
Alejandra Siles <alejandra_siles_a@hotmail.com>
2016-06-27 09:52:43 -0400

    Testeo nombres servicios y empleados

---
+++ src/test/java/bo/edu/ucbcba/hotel/model/EmployersTest.java
@@ -0,0 +1,48 @@
+package bo.edu.ucbcba.hotel.model;
+import bo.edu.ucbcba.hotel.exceptions.ValidationException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Alejandra on 27/06/2016.
+ */
+public class EmployersTest {
+    private Employers employer;
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Before
+    public void setUp() {
+        employer = new Employers();
+    }
+
+    @Test
+    public void testSetName() {
+        employer.setName("Good name");
+        assertEquals("Good name", employer.getName();
+    }
+
+    @Test
+    public void testSetNoName() {
+        exception.expect(ValidationException.class);
+        exception.expectMessage("Name's employer can't be empty");
+        employer.setName("");
+    }
+
+    @Test
+    public void testSetNullName() {
+        exception.expect(ValidationException.class);
+        exception.expectMessage("Null Name");
+        employer.setName(null);
+    }
+
+    @Test
+    public void testSetLongName() {
+        exception.expect(ValidationException.class);
+        exception.expectMessage("Name's employer is too long");
+        employer.setName(new String(new char[256]).replace('\0', 'a'));
+    }
+}
---
+++ src/test/java/bo/edu/ucbcba/hotel/model/ServicesTest.java
@@ -0,0 +1,50 @@
+package bo.edu.ucbcba.hotel.model;
+
+import bo.edu.ucbcba.hotel.exceptions.ValidationException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Alejandra on 27/06/2016.
+ */
+public class ServicesTest {
+    private Services service;
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Before
+    public void setUp() {
+        service = new Services();
+    }
+
+    @Test
+    public void testSetName() {
+        service.setName("Good name");
+        assertEquals("Good name", service.getName();
+    }
+
+    @Test
+    public void testSetNoName() {
+        exception.expect(ValidationException.class);
+        exception.expectMessage("Name' service can't be empty");
+        service.setName("");
+    }
+
+    @Test
+    public void testSetNullName() {
+        exception.expect(ValidationException.class);
+        exception.expectMessage("Null Name");
+        service.setName(null);
+    }
+
+    @Test
+    public void testSetLongName() {
+        exception.expect(ValidationException.class);
+        exception.expectMessage("Name's service is too long");
+        service.setName(new String(new char[256]).replace('\0', 'a'));
+    }
+}
